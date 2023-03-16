45bf44e9560a98b0ade5c9d40a3e6a0808d227ca
AmanJemal <amanjemal1998@gmail.com>
2023-03-16 20:49:06 +0000

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
-
 /**
- * main -  Entry pointt
- *
- * Return: Always 0 (success)
- */  	
+   * main - Entry point
+    *
+     * Return: Always 0 (Success)
+      */
 int main(void)
 {
 	char str[] = "Programming is like building a multilingual puzzle\n";
