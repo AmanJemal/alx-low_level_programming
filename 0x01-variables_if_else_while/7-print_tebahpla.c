5f060aca27840461e16d47ce38193d7dda26435e
AmanJemal <amanjemal1998@gmail.com>
2023-03-17 13:52:05 -0700

    Adding file 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - prinys lowercase alphabets except e and q
+ *
+ * Return: 0 is for success
+ */
+int main(void)
+{
+
+	int i = 122;
+
+	while (i > 96)
+	{	
+		putchar(i);		
+		i--;
+	}
+	putchar('\n');
+	return (0);
+}
