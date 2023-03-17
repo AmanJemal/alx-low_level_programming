fd2badf8e5412cfc06d451adf06e4471c655ac30
AmanJemal <amanjemal1998@gmail.com>
2023-03-17 13:02:04 -0700

    Adding file 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - determines to print single digit  numbers
+ *
+ * Return: 0 is for success
+ */
+int main(void)
+{
+
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d\n", i);
+		i++;
+	}
+	return (0);
+}
