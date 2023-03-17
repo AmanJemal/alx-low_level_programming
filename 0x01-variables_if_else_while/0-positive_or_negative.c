e2a438b0318b51727bf196fe3439d1f94f130f29
AmanJemal <amanjemal1998@gmail.com>
2023-03-17 07:21:53 -0700

    correctiong betty-doc for main function

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - coditioning if the number is positive , zeo or negatine
+ *
+ * Return: Always 0 (success(
+ */
 int main(void)
 {
 	int n;
