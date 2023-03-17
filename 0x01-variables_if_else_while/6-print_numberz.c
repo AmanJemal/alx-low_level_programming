82bdf48e6ce0986b8e2ef3e5a5201665dd1b66ee
AmanJemal <amanjemal1998@gmail.com>
2023-03-17 13:19:45 -0700

    Adding file 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers using putchar
+ *
+ * Return: 9 is for success
+ */
+int main(void)
+{
+
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
