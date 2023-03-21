31869b219730bfb40ddfa705f2a4639defa36f33
AmanJemal <amanjemal1998@gmail.com>
2023-03-21 17:11:21 +0000

    Addig file to print alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - the code start execute from main function
+ * Return: 0 is Always for Success
+ */
+void print_alphabet_x10(void);
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
