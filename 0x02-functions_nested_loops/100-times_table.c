caf166e30b30d84a9e0c51c6693376f29ed32972
AmanJemal <amanjemal1998@gmail.com>
2023-03-22 17:00:00 +0000

    correction

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -41,7 +41,7 @@ void print_times_table(int n)
 					else if (pr >= 100)
 					{
 						_putchar('0' + (pr / 10));
-						_putchar('0' + (pr / 10) % 10);
+						_putchar('0' + ((pr / 10) % 10));
 						_putchar('0' + (pr % 10));
 					}
 				}
