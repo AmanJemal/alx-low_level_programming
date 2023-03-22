e21c5ed5046582c342305e3a355390531a68d4cc
AmanJemal <amanjemal1998@gmail.com>
2023-03-22 12:35:23 +0000

    correction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -32,7 +32,7 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar('0' + (pr / 10));
-				_putchar('9' + (pr % 10));
+				_putchar('0' + (pr % 10));
 			}
 		}
 		_putchar('\n');
