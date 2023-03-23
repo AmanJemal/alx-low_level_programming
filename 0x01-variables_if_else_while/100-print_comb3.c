bf5ed9ebc1bed7e6d5071cc922d98012bea13e20
AmanJemal <amanjemal1998@gmail.com>
2023-03-23 06:26:35 +0000

    Adding 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ *main - print 00 to 99 with no duplicate digits or combos: no 11, no 10 (01)
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i <= '9'; i++) 
+	{
+		for (j = (i + 1); j <= '9'; j++) 
+		{
+			putchar(i);
+			putchar(j);
+
+			if (i != '8' || j != '9') 
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
