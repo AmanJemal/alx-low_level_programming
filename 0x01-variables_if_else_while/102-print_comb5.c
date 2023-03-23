5e37f045b164724224af916a49ffdedc469d58b6
AmanJemal <amanjemal1998@gmail.com>
2023-03-23 07:00:58 +0000

    correction

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,26 +7,26 @@
 
 int main(void)
 {
-	int i;
-	int j;
+	int tens;
+	int ones;
 	int t;
 	int o;
 
-	for (i = '0'; i <= '9'; i++)
+	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
 	{
-		for (j = '0'; j <= '9'; j++)
+		for (ones = '0'; ones <= '9'; ones++)
 		{
-			for (t = i; t <= '9'; t++)
+		 	for (t = tens; t <= '9'; t++) /*print second of pair*/
 			{
-				for (o = j + 1; o <= '9'; o++)
+				for (o = ones + 1; o <= '9'; o++)
 				{
-					putchar(i);
-					putchar(j);
+					putchar(tens);
+					putchar(ones);
 					putchar(' ');
 					putchar(t);
 					putchar(o);
 
-					if (!((i == '9' && j == '8') &&
+					if (!((tens == '9' && ones == '9') &&
 					      (t == '9' && o == '9')))
 					{
 						putchar(',');
