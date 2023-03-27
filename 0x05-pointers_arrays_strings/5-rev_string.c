99a7a868c25a954cfb181af2af4407621a1bb4a8
AmanJemal <amanjemal1998@gmail.com>
2023-03-27 19:40:02 +0000

    Adding 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - a function that male a string in reverse
+ * @s: Declared as pointer of type char.
+ */
+void rev_string(char *s)
+{
+	int i, len;
+
+	if (s == NULL)
+	{
+		return;
+	}
+	
+	len = strlen(s);
+
+	for (i = 0; i < len / 2; i++)
+	{
+		char tmp = s[i];
+		s[i] = s[len - i -1];
+		s[len - i - 1] = tmp;
+	}
+
+}
