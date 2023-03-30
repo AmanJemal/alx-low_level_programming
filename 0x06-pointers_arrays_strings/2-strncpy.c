b5de150f7ea17d1ec3ade4af3174d42d5642b89d
AmanJemal <amanjemal1998@gmail.com>
2023-03-30 04:39:47 +0000

    correction

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,8 +13,11 @@ char *_strncpy(char *dest, char *src, int n)
 	while (i < n && *(src + i) != '\0')
 	{
 		*(dest + i) = *(src + i);
-		if (*(src + i) == '\0')
-			break;
+		i++;
+	}
+	while (i < n)
+	{
+		*(dest + i) = '\0';
 		i++;
 	}
 	return (dest);
