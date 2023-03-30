68014c25e007becbd4f113581d4ec0ff1209d0cd
AmanJemal <amanjemal1998@gmail.com>
2023-03-30 03:50:02 +0000

    correction

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,24 +5,26 @@
  * @dest: destination string declared as pointer of type char
  * @src: source string declared as pointer of type char
  * @n: number to count charcter.
- * Return: pointer ptr
+ * Return: pointer to dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int  i, j;
 
 	i = 0;
+	j = 0;
 
 	while (*(dest + i) != '\0')
 	{
 		i++;
 	}
-	for (j = 0; j < n; j++)
+	while (j < n)
 	{
 		*(dest + i) = *(src + j);
 		if (*(src + j) == '\0')
 			break;
 		i++;
+		j++;
 	}
 	return (dest);
 }
