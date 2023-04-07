b1d3cce9e9fe4e5fbd95338e986ceaba274cf552
AmanJemal <amanjemal1998@gmail.com>
2023-04-07 16:42:14 +0000

    Adding 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - a func returns the natural square root of a number.
+ * @n: input number.
+ * Return: natural square root.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (exponent_opr(n, 2));
+}
+
+/**
+ * exponent_opr - a func returns the natural square root of a number.
+ * @n: input number.
+ * @i: iterator.
+ * Return: square root or -1.
+ */
+int exponent_opr(int n, int i)
+{
+	if (i % (n / i) == 0)
+	{
+		if (i * (n / i) == n)
+			return (i);
+		else
+			return (-1);
+	}
+	return (0 + exponent_opr(n, i + 1));
+}
+
