a99d8724f3029d9c6385dbbc054e90642d6cb916
AmanJemal <amanjemal1998@gmail.com>
2023-05-08 17:30:23 +0000

    correcction on output

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -7,35 +7,6 @@
 #include <elf.h>
 
 /**
- * print_type - prints  the type.
- * @ptr: pointer type of character.
- * Return: no return.
- */
-void print_type(char *ptr)
-{
-	char type = ptr[16];
-
-	if (ptr[5] == 1)
-		type = ptr[16];
-	else
-		type = ptr[17];
-
-	printf("  Type:                              ");
-	if (type == 0)
-		printf("NONE (No file type)\n");
-	else if (type == 1)
-		printf("REL (Relocatable file)\n");
-	else if (type == 2)
-		printf("EXEC (Executable file)\n");
-	else if (type == 3)
-		printf("DYN (Shared object file)\n");
-	else if (type == 4)
-		printf("CORE (Core file)\n");
-	else
-		printf("<unknown: %x>\n", type);
-}
-
-/**
  * print_addr - prints the address
  * @ptr: pointer type of character.
  * Return: no return.
@@ -83,6 +54,34 @@ void print_addr(char *ptr)
 	printf("\n");
 }
 
+/**
+ * print_type - prints  the type.
+ * @ptr: pointer type of character.
+ * Return: no return.
+ */
+void print_type(char *ptr)
+{
+	char type = ptr[16];
+
+	if (ptr[5] == 1)
+		type = ptr[16];
+	else
+		type = ptr[17];
+
+	printf("  Type:                              ");
+	if (type == 0)
+		printf("NONE (No file type)\n");
+	else if (type == 1)
+		printf("REL (Relocatable file)\n");
+	else if (type == 2)
+		printf("EXEC (Executable file)\n");
+	else if (type == 3)
+		printf("DYN (Shared object file)\n");
+	else if (type == 4)
+		printf("CORE (Core file)\n");
+	else
+		printf("<unknown: %x>\n", type);
+}
 
 
 /**
